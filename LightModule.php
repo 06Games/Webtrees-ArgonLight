bd011902fd6153ced264c8fe6921df14f5a83fa5
EvanG <evan_g@orange.fr>
2022-11-27 17:46:35 +0100

    1.1.1

--- LightModule.php
+++ LightModule.php
@@ -1,54 +1,67 @@
 <?php
 /************************************************
-                Argon Light Theme
-             Originally made by jchue
-  Under the Internet Systems Consortium License
-  https://github.com/jchue/argon-webtrees-theme
-
-       Forked and maintained by Evan Galli
-             Under the same licence
- https://github.com/06Games/Webtrees-ArgonLight
-***********************************************/
+ * Argon Light Theme
+ * Originally made by jchue
+ * Under the Internet Systems Consortium License
+ * https://github.com/jchue/argon-webtrees-theme
+ *
+ * Forked and maintained by Evan Galli
+ * Under the same licence
+ * https://github.com/06Games/Webtrees-ArgonLight
+ ***********************************************/
 
 declare(strict_types=1);
 
 namespace EvanG\Themes\LightTheme;
 
+use Fisharebest\Webtrees\Module\AbstractModule;
 use Fisharebest\Webtrees\Module\ModuleCustomInterface;
 use Fisharebest\Webtrees\Module\ModuleCustomTrait;
-use Fisharebest\Webtrees\Module\AbstractModule;
 use Fisharebest\Webtrees\Module\ModuleThemeInterface;
 use Fisharebest\Webtrees\Module\ModuleThemeTrait;
 use Fisharebest\Webtrees\View;
 
-class LightTheme extends AbstractModule implements ModuleCustomInterface, ModuleThemeInterface {
+class LightTheme extends AbstractModule implements ModuleCustomInterface, ModuleThemeInterface
+{
     use ModuleCustomTrait, ModuleThemeTrait;
 
     public function title(): string { return 'Argon Light'; }
+
     public function description(): string { return 'A light theme for Webtrees based on Argon Design System'; }
+
     public function customModuleAuthorName(): string { return 'EvanG'; }
-    public function customModuleVersion(): string { return '1.1.0'; }
+
+    public function customModuleVersion(): string { return '1.1.1'; }
+
+    public function customModuleLatestVersionUrl(): string { return 'https://github.com/06Games/Webtrees-ArgonLight/raw/main/version.txt'; }
+
     public function customModuleSupportUrl(): string { return 'https://github.com/06Games/Webtrees-ArgonLight/issues'; }
-    public function customModuleLatestVersion(): string { return 'https://github.com/06Games/Webtrees-ArgonLight/releases/latest'; }
-    public function resourcesFolder(): string { return __DIR__ . '/resources/'; }
-    public function stylesheets(): array { return [$this->assetUrl('css/imports.css'), $this->assetUrl('css/theme.css'), $this->assetUrl('css/magicsunday-fan-chart.css')]; }
+
+    public function stylesheets(): array
+    {
+        return [
+            $this->assetUrl('css/imports.css'),
+            $this->assetUrl('css/theme.css'),
+            $this->assetUrl('css/magicsunday-fan-chart.css')
+        ];
+    }
 
     public function parameter($parameter_name)
     {
         $parameters = [
-            'chart-background-f'             => 'fff4f9',
-            'chart-background-m'             => 'f4fdff',
-            'chart-background-u'             => 'f4f5f7',
-            'chart-box-x'                    => 260,
-            'chart-box-y'                    => 85,
-            'chart-font-color'               => '000000',
-            'chart-spacing-x'                => 5,
-            'chart-spacing-y'                => 10,
-            'compact-chart-box-x'            => 240,
-            'compact-chart-box-y'            => 50,
+            'chart-background-f' => 'fff4f9',
+            'chart-background-m' => 'f4fdff',
+            'chart-background-u' => 'f4f5f7',
+            'chart-box-x' => 260,
+            'chart-box-y' => 85,
+            'chart-font-color' => '000000',
+            'chart-spacing-x' => 5,
+            'chart-spacing-y' => 10,
+            'compact-chart-box-x' => 240,
+            'compact-chart-box-y' => 50,
             'distribution-chart-high-values' => '84beff',
-            'distribution-chart-low-values'  => 'c3dfff',
-            'distribution-chart-no-values'   => 'ffffff',
+            'distribution-chart-low-values' => 'c3dfff',
+            'distribution-chart-no-values' => 'ffffff',
         ];
 
         return $parameters[$parameter_name];
@@ -89,8 +102,11 @@ class LightTheme extends AbstractModule implements ModuleCustomInterface, Module
 
 
         /** My views **/
+        
         View::registerCustomView('::lists/individuals-table', $this->name() . '::lists/individuals-table');
         View::registerCustomView('::lists/families-table', $this->name() . '::lists/families-table');
         View::registerCustomView('::individual-page-menu', $this->name() . '::individual-page-menu');
     }
+
+    public function resourcesFolder(): string { return __DIR__ . '/resources/'; }
 }
